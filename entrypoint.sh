d9a5c21a753ba1e37e317d9918bbe80bc779ce14
sshniro <sshniro@gmail.com>
2020-03-06 13:09:11 +0100

    Adding additional parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 ls
 # Runs the ZAP Baseline scan
-su docker run -v $(pwd):/zap/wrk/:rw  -t owasp/zap2docker-stable zap-baseline.py  -t https://www.example.com -g gen.conf -J report_json.json
+docker run -v $(pwd):/zap/wrk/:rw  -t owasp/zap2docker-stable zap-baseline.py  -t https://www.example.com -g gen.conf -J report_json.json
 # listing all the variables
 pwd && ls -l /zap/wrk/
 # Post process the generated report and create issues
